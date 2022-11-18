adbf740e47433d0e6b09ca27e8b09eeaf6433520
grazicristini <grazielicristhyny2002@gmail.com>
2022-11-18 11:45:53 +0000

    Select Genero

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once './vendor/autoload.php';
+use PDO;
+
+$bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+
+$comando = $bd->prepare('SELECT * FROM generos');
+$comando->execute();
+$generos = $comando->fetchAll(PDO::FECH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+        </tr>
+        <?php foreach($generos as $g): ?>
+            <tr>
+                <td><?= $g['id'] ?></td>
+                <td><?= $g['nome'] ?></td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+</html>
