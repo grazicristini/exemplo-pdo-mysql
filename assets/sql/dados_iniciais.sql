afc17457c36dad108fab9589997863a9335394af
grazicristini <grazielicristhyny2002@gmail.com>
2022-11-18 11:43:13 +0000

    Preparação da Base de Dados

---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO generos(id, nome) VALUES
+    (1, 'Alta Fantasia'),
+    (2, 'Polícial'),
+    (3, 'Drama'),
+    (4, 'ficção Científica');
+
+INSERT INTO livros(id, titulo, id_genero) VALUES
+    (1, 'Assassinato no Expresso do Oriente', 2),
+    (2, 'A sociedade do Anel', 1),
+    (3, 'Come Eu Era Antes de Você', 3),
+    (4, 'O Guia do Mochileiro da Galáxias', 4);
