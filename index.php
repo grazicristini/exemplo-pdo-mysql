7f6ad895cf806f977aa6321bfe21c8f7d02274f5
grazicristini <grazielicristhyny2002@gmail.com>
2022-11-25 12:03:18 +0000

    Delete Genero

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ $generos = $comando->fetchAll(PDO::FECH_ASSOC);
                 <td><?= $g['nome'] ?></td>
                 <td>
                     <a href="update.php?id=<?= $g['id'] ?>">Editar</a>
+                    <a href="delete.php?id=<?= $g['id'] ?>">Excluir</a>
                 </td>
             </tr>
         <?php endforeach ?>
