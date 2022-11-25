b5fc56c191daa7568365c13ade0af8123f941414
grazicristini <grazielicristhyny2002@gmail.com>
2022-11-25 15:39:02 +0000

    barra de navegação

--- includes/header.php
+++ includes/header.php
@@ -9,4 +9,21 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Biblioteca</a>
+            <button class="navbar-togler" type="buton" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-contols="navbarSupportedContent"
+                aria-expanded="false" arialabel="Toggle navigation">
+                <span class="navbar-toggle-icon"></span>
+            </button>
+            <div class="collapse navbar-colapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/index.php">Generos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<br />
     <main class="container">
